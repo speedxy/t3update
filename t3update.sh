acfd3c6a23dded20005e9545b3677c521146d23f
speedxy <swat@parrot-media.de>
2016-01-21 22:39:30 +0100

    Update t3update.sh

--- t3update.sh
+++ t3update.sh
@@ -1,7 +1,12 @@
-REMOTE_ROOT=/var/www/webX/
-REMOTE_DIR=/var/www/webX/html/domainname.de/
+REMOTE_ROOT=/var/www/webX
+REMOTE_DIR=/var/www/webX/html/domainname.de
 
-# Backup anfertigen
+# Create backup
+mkdir -p $REMOTE_ROOT/files/update_backups
+tar -zcf backup.tar.gz *
+echo "Backup erfolgreich angelegt ($REMOTE_ROOT/files/update_backups/backup.tar.gz)."
+
+# Download sources
 cd $REMOTE_ROOT/files/
 wget http://prdownloads.sourceforge.net/typo3/typo3_src-6.2.17.tar.gz
 tar -zxf typo3_src*.tar.gz
