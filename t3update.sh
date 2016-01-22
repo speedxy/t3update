db2498f4e1eaa075ba8a9be8806fa574b853838d
speedxy <swat@parrot-media.de>
2016-01-22 09:02:38 +0100

    Update t3update.sh

--- t3update.sh
+++ t3update.sh
@@ -9,6 +9,8 @@ echo "Backup erfolgreich angelegt ($REMOTE_ROOT/files/update_backups/backup.tar.
 # Download sources
 cd $REMOTE_ROOT/files/
 wget http://prdownloads.sourceforge.net/typo3/typo3_src-6.2.17.tar.gz
-tar -zxf typo3_src*.tar.gz
+
+# Unpack sources and overwrite old version
 rm typo3_src*.tar.gz
-mv typo3_src*
+tar -zxf typo3_src*.tar.gz -C $REMOTE_DIR --strip 1
+rm typo3_src*.tar.gz
